9b8d75d6acf92d5fc2613c6961e95c503670ec8e
Vivekanandan Ramalingam <vir@stanford.edu>
2022-06-26 14:49:03 -0700

    fix run modisco script to run sherlock

--- sherlock/modisco/submit_modisco_jobs.sh
+++ sherlock/modisco/submit_modisco_jobs.sh
@@ -4,7 +4,7 @@ experiment=$1
 round=$2
 oak_dir=$3
 
-if ![[ -f /scratch/users/vir/my_scripts ]]; then
+if ! [[ -f /scratch/users/vir/my_scripts ]]; then
     cd /scratch/users/vir/; rm -rf my_scripts; mkdir my_scripts
     cd /scratch/users/vir/my_scripts
     git clone --depth 1 --branch main https://github.com/viramalingam/tf-atlas-pipeline.git
@@ -16,7 +16,7 @@ if [ -f $oak_dir/shap/$round/$experiment/profile_scores.h5 ] && [ -f $oak_dir/sh
     if [ -f $oak_dir/modisco/$round/$experiment/profile/modisco_results.h5 ] && [ -f $oak_dir/modisco/$round/$experiment/counts/modisco_results.h5 ] ; then
         echo "modisco run exists"
     else
-        if ![[ -f $oak_dir/modisco/$round/$experiment ]]; then
+        if ! [[ -f $oak_dir/modisco/$round/$experiment ]]; then
             mkdir $oak_dir/modisco/$round/$experiment
         fi
         modisco_dir=$oak_dir/modisco/$round/$experiment
