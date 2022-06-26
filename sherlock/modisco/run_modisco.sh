ec042b984c25cb479fc555088553ca4f7a0683c1
Vivekanandan Ramalingam <vir@stanford.edu>
2022-06-26 14:37:17 -0700

    fix run modisco script to run sherlock

--- sherlock/modisco/run_modisco.sh
+++ sherlock/modisco/run_modisco.sh
@@ -3,16 +3,10 @@ shap=$2
 max_seqlets=$3
 modisco_dir=$4
 
-cd /scratch/users/vir/; rm -rf my_scripts; mkdir my_scripts
-cd /scratch/users/vir/my_scripts
-git clone --depth 1 --branch main https://github.com/viramalingam/tf-atlas-pipeline.git
-chmod -R 777 tf-atlas-pipeline
-cd tf-atlas-pipeline/anvil/modisco/
-
 ##modisco
 
-echo "run /scratch/users/vir/my_scripts/tf-atlas-pipeline/sherlock/modisco/modisco_pipeline.sh" ${experiment} ${shap_dir} ${max_seqlets}
-/scratch/users/vir/my_scripts/tf-atlas-pipeline/sherlock/modisco/modisco_pipeline.sh ${experiment} ${shap_dir} ${max_seqlets}
+echo "run /scratch/users/vir/my_scripts/tf-atlas-pipeline/sherlock/modisco/modisco_pipeline.sh" ${experiment} ${shap_dir} ${max_seqlets} /scratch/users/vir/$experiment/
+/scratch/users/vir/my_scripts/tf-atlas-pipeline/sherlock/modisco/modisco_pipeline.sh ${experiment} ${shap_dir} ${max_seqlets} /scratch/users/vir/$experiment/
 
 echo "copying all files to cromwell_root folder"
 
--- sherlock/modisco/submit_modisco_jobs.sh
+++ sherlock/modisco/submit_modisco_jobs.sh
@@ -4,6 +4,12 @@ experiment=$1
 round=$2
 oak_dir=$3
 
+cd /scratch/users/vir/; rm -rf my_scripts; mkdir my_scripts
+cd /scratch/users/vir/my_scripts
+git clone --depth 1 --branch main https://github.com/viramalingam/tf-atlas-pipeline.git
+chmod -R 777 tf-atlas-pipeline
+cd tf-atlas-pipeline/anvil/modisco/
+
 if [[ -d $oak_dir/$experiment ]] ; then
     if [[ -f $oak_dir/shap/$round/$experiment/profile_scores.h5 ] && [ -f $oak_dir/shap/$round/$experiment/counts_scores.h5 ]] ; then
         if [[ -f $oak_dir/modisco/$round/$experiment/profile/modisco_results.h5 ] && [ -f $oak_dir/modisco/$round/$experiment/counts/modisco_results.h5 ]] ; then
@@ -20,7 +26,7 @@ if [[ -d $oak_dir/$experiment ]] ; then
                 -t '12:00:00' -c 10 --mem=60G \
                 -o $modisco_dir/output_log.txt \
                 -e $modisco_dir/error_log.txt \
-                run_modisco.sh  $experiment $oak_dir/shap/$round/$experiment 20000 $modisco_dir /scratch/users/vir/$experiment/
+                run_modisco.sh  $experiment $oak_dir/shap/$round/$experiment 20000 $modisco_dir
         fi
     else
         echo "do interpretation step first"
