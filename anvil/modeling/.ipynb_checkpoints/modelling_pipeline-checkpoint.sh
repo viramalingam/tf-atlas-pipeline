bd1b9dbaf1e24f977295f46d1c7ad42dc0f3189a
Vivekanandan Ramalingam <vir@stanford.edu>
2022-08-19 16:35:04 -0700

    troubleshooting compatiblity with chroms-wise training

--- anvil/modeling/.ipynb_checkpoints/modelling_pipeline-checkpoint.sh
+++ anvil/modeling/.ipynb_checkpoints/modelling_pipeline-checkpoint.sh
@@ -175,6 +175,7 @@ echo $( timestamp ): "cp" $splits_json \
 $project_dir/splits.json | tee -a $logfile 
 cp $splits_json $project_dir/splits.json
 
+echo $indices_files
 # cp train val test indices files
 if [[ -v $indices_files ]];then
     if [[ $indices_files!='' ]];then
