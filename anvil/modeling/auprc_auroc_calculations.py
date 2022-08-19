9756041ba33a68e23fd3660ed1e66593328fb899
Vivekanandan Ramalingam <vir@stanford.edu>
2022-08-19 14:09:50 -0700

    big fix

--- anvil/modeling/auprc_auroc_calculations.py
+++ anvil/modeling/auprc_auroc_calculations.py
@@ -5,6 +5,11 @@ import csv
 import h5py
 import pandas as pd
 
+def none_or_str(value):
+    if value == "None":
+        return None
+    return value
+
 def metrics_argsparser():
     parser=argparse.ArgumentParser(description="generate ")
     parser.add_argument("--h5_file",required=True, help="h5 file containing the predictions")
@@ -13,6 +18,9 @@ def metrics_argsparser():
     parser.add_argument("-o","--output_dir", required=True, help="directory for the output")
     parser.add_argument("--chroms", nargs='+', required=True, help="test chromosome")
     parser.add_argument("--output_length", type=int, default=1000, help="negative bed file")
+    parser.add_argument('--test-indices-file', type=none_or_str,
+                        help="path to indices file to filter the peaks and background combined dataframe for prediction.", default=None)
+
     return parser
 
 parser = metrics_argsparser()
@@ -33,7 +41,30 @@ h5_df = pd.DataFrame({'chroms':f['coords']['coords_chrom'][()],
                      'log_counts':(f['predictions']['pred_logcounts'][()][:,0]).tolist()})
 
 predictions = pd.merge(all_regions_df, h5_df, how='inner', on=['chroms','start','end'])
-predictions = predictions[predictions['chroms'].isin(args.chroms)].reset_index(drop=True)
+
+if args.test_indices_file!=None:
+        # make sure the background_train_indices_file file exists
+        if not os.path.isfile(args.test_indices_file):
+            raise NoTracebackException(
+                "File not found: {} ".format(test_indices_file))    
+
+        # load test_indices
+        f = open(args.test_indices_file)
+        lines = f.readlines()
+        test_indices = [int(line.rstrip('\r').rstrip('\n'))
+                                  for line in lines]
+        f.close()
+    else:
+        test_indices = None
+    
+            
+if args.chroms == ['None']:
+    args.chroms = None
+        
+if args.chroms!=None:
+    predictions = predictions[predictions['chroms'].isin(args.chroms)].reset_index(drop=True)
+if test_indices!=None:
+    predictions = predictions.loc[predictions.index[test_indices]].reset_index(drop=True)
 
 from sklearn.metrics import roc_auc_score, average_precision_score
 
