008f2fd8868fbaf34668e82558390373a7792a4e
Vivekanandan Ramalingam <vir@stanford.edu>
2024-01-08 15:18:25 -0800

    big fig importance h5 to bw file; fixed help; added debug-chr variable

--- anvil/shap/importance_hdf5_to_bigwig.py
+++ anvil/shap/importance_hdf5_to_bigwig.py
@@ -7,12 +7,13 @@ import hdf5plugin
 
 # need full paths!
 parser = argparse.ArgumentParser(description="Convery importance scores in hdf5 format to bigwig. PROVIDE ABSOLUTE PATHS!")
-parser.add_argument("-h5", "--hdf5", type=str, required=True, help="HDF5 file f such that f['projected_shap']['seq'] has (N x 4 x seqlen) shape with importance score * sequence so that at each f['projected_shap']['seq'][i, :, j] has 3 zeros and 1 non-zero value")
-parser.add_argument("-r", "--regions", type=str, required=True, help="10 column BED file of length = N which matches f['projected_shap']['seq'].shape[0]. The ith region in the imBED file corresponds to ith entry in importance matrix. If start=2nd col, summit=10th col, then the importance scores are for [start+summit-(seqlen/2):start+summit+(seqlen/2)]")
+parser.add_argument("-h5", "--hdf5", type=str, required=True, help="HDF5 file f such that f['hyp_scores'] has (N x 4 x seqlen) shape with importance score * sequence so that at each f['input_seqs'] has 3 zeros and 1 non-zero value")
+parser.add_argument("-r", "--regions", type=str, required=True, help="10 column BED file of length = N which matches f['input_seqs'].shape[0]. The ith region in the imBED file corresponds to ith entry in importance matrix. If start=2nd col, summit=10th col, then the importance scores are for [start+summit-(seqlen/2):start+summit+(seqlen/2)]")
 parser.add_argument("-c", "--chrom_sizes", type=str, required=True, help="Chromosome sizes 2 column file")
 parser.add_argument("-o", "--outfile", type=str, required=True, help="Output bigwig file")
 parser.add_argument("-s", "--outstats", type=str, required=True, help="Output file with stats of low and high quantiles")
 parser.add_argument("-t", "--tqdm", type=int,default=0, help="Use tqdm")
+parser.add_argument("-d", "--debug-chr", type=str, default=None, help="Run for one chromosome only (e.g. chr12) for debugging")
 parser.add_argument("-z", "--gzipped", action='store_true', help="peak file is gzipped")
 
 args = parser.parse_args()
